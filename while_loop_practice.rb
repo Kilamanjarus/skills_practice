ed742dd4adca43fae4a9114396e75355b917e0e0
max <max.lillie95@gmail.com>
2022-07-29 09:56:31 -0500

    added loops practice from core prgramming

---
+++ while_loop_practice.rb
@@ -0,0 +1,69 @@
+#1
+index = 1
+while index <= 10
+  p index
+  index += 1
+end
+
+#2
+index = 0
+while index < 5
+  p "hello"
+  index += 1
+end
+
+#3
+word = ""
+while word.downcase != "stop"
+  p "Please enter a word:"
+  word = gets.chomp
+end
+
+#4
+index = 0
+while index <= 100
+  p index
+  index += 5
+end
+
+#5
+index = 0
+while index < 10
+  p "9000"
+  index += 1
+end
+
+#6
+num = 0
+while num <= 10
+  p "Please enter a number:"
+  num = gets.chomp.to_i
+end
+
+#7
+index = 50
+while index < 71
+  p index
+  index += 1
+end
+
+#8
+index = 0
+while index < 144
+  p "Around the world"
+  index += 1
+end
+
+#9
+word = ""
+while word.length < 5
+  p "Please enter a word:"
+  word = gets.chomp
+end
+
+#10
+index = 2
+while index <= 40
+  p index
+  index += 2
+end
